9e7527abe52f59638ce7c2253c362f1cabc657c6
Bruno Pereyra <brunopereyra459@gmail.com>
2025-05-08 22:25:14 -0300

    credenciales de development

--- front-end/services/JobsService.ts
+++ front-end/services/JobsService.ts
@@ -1,5 +1,6 @@
 import Constants from "expo-constants";
-const API = "http://192.168.0.28:9000";
+const API = Constants.expoConfig?.extra?.EXPO_URL_API ?? "http://192.168.0.28:9000";
+
 export interface Job {
     id: string;
     title: string;
--- front-end/services/chatService.ts
+++ front-end/services/chatService.ts
@@ -1,6 +1,7 @@
 import Constants from "expo-constants";
 
-const API = "http://192.168.0.28:9000";
+const API = Constants.expoConfig?.extra?.EXPO_URL_API ?? "http://192.168.0.28:9000";
+
 
 export interface ChatRoom {
     id: string;
